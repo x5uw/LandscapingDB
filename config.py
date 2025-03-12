fe2e966c3ee17a4376ba0913b8a5194cf3d2915b
x5uw <168318933+x5uw@users.noreply.github.com>
2025-03-11 23:27:43 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+# Global database configuration settings.
+DB_CONFIG = {
+    "host": "localhost",
+    "dbname": "landscapedb",
+    "user": "postgres",
+    "password": "**YOUR PASSWORD HERE**"  # Replace with your actual password
+}
