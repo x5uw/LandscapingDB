3d082458cbfab5e09bded0a162abf796e4e94d02
mistermt04 <mistermt2004@gmail.com>
2025-03-19 03:06:14 -0700

    fixed my api bugs and also interaction with listassignedservices

--- config.example.py
+++ config.example.py
@@ -5,5 +5,5 @@ DB_CONFIG = {
     "host": "localhost",
     "dbname": "landscapedb",
     "user": "postgres",
-    "password": "MinhTin1217",  # Replace with your actual password
+    "password": "XXXXXX",  # Replace with your actual password
 }
