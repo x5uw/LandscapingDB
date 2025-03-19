e5dbe302fd7bfc09a83bd4ccb446c3b3db2d248f
x5uw <168318933+x5uw@users.noreply.github.com>
2025-03-19 12:04:23 -0700

    Create config.example.py
    example config file for each developer to have personalized local configurations

---
+++ config.example.py
@@ -0,0 +1,9 @@
+# config.py
+
+# Global database configuration settings.
+DB_CONFIG = {
+    "host": "localhost",
+    "dbname": "landscapedb",
+    "user": "postgres",
+    "password": "**PASSWORD**",  # Replace with your actual password
+}
