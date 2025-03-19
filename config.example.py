00448618cca760a0d9a8e06254228b4aaa8ea97b
x5uw <x5@uw.edu>
2025-03-19 12:18:34 -0700

    making changes to config.example.py to prevent conflicts

--- config.example.py
+++ config.example.py
@@ -1,9 +1,11 @@
-# config.py
+# config.example.py
+
+# create a copy of this file as "config.py" and replace the password with your actual password
 
 # Global database configuration settings.
-DB_CONFIG = {
-    "host": "localhost",
-    "dbname": "landscapedb",
-    "user": "postgres",
-    "password": "**PASSWORD**",  # Replace with your actual password
-}
+#DB_CONFIG = {
+#    "host": "localhost",
+#    "dbname": "landscapedb",
+#    "user": "postgres",
+#    "password": "**PASSWORD**",  # Replace with your actual password on your own local file "config.py"
+#}
